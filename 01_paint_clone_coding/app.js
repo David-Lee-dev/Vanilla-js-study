05f39b2a40afe95b2291f79639749e66989d0957
Ju Hyeon <aganga7427@gmail.com>
2021-10-27 20:11:05 +0900

    Add fill canvas

--- 01_paint_clone_coding/app.js
+++ 01_paint_clone_coding/app.js
@@ -2,13 +2,17 @@ const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext("2d")
 const colors = document.getElementsByClassName("jsColor")
 const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
+const INITIAL_COLOR = "#2c2c2c"
 
 canvas.width = 500
 canvas.height = 700
-ctx.strokeStyle = "#2c2c2c"
+ctx.strokeStyle = INITIAL_COLOR
+ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5
 
 let painting = false
+let filling = false
 
 function startPaing() {
   painting = true
@@ -39,6 +43,7 @@ function onMouseLeave(event) {}
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor
   ctx.strokeStyle = color
+  ctx.fillStyle = color
 }
 
 function handleRangeChange(event) {
@@ -46,16 +51,37 @@ function handleRangeChange(event) {
   ctx.lineWidth = val
 }
 
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false
+    mode.innerText = "Fill"
+  } else {
+    filling = true
+    mode.innerText = "Paint"
+  }
+}
+
+function handleCanvasClick() {
+  if (filling === true) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  }
+}
+
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', startPaing)
   canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
+  canvas.addEventListener('click', handleCanvasClick)
 }
 if (colors) {
   Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
 }
 if (range) {
   range.addEventListener("input", handleRangeChange)
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick)
 }
 // Array.from 메서드는 객체로부터 배열을 만든다
